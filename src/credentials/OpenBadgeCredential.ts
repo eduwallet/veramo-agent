a1ee1c545e9e709a513137fbbe302022ed1e698d
Bèr Kessels <ber@berk.es>
2024-11-20 11:54:04 +0100

    feat: Use achievement name and description for credential name and description

--- src/credentials/OpenBadgeCredential.ts
+++ src/credentials/OpenBadgeCredential.ts
@@ -1,39 +1,11 @@
 import { CredentialDataSupplierArgs, CredentialDataSupplierResult } from "@sphereon/oid4vci-issuer";
 import { ICredential } from "@sphereon/ssi-types"
-import { Issuer } from "issuer/Issuer";
 import { BaseCredential } from "./BaseCredential";
 import { debug } from "utils/logger";
 
 export interface DisplayConfiguration {
   name: string;
   description: string;
-}
-
-export interface CredentialConfiguration {
-  display: DisplayConfiguration[];
-  getFirstDisplay: () => DisplayConfiguration;
-}
-
-const defaultConfiguration: CredentialConfiguration = {
-  display: [{
-    name: '',
-    description: '',
-  }],
-
-  getFirstDisplay: function() {
-    return this.display[0];
-  },
-};
-
-// the issuer.getCredentialConfiguration has a return type of CredentialConfiguration | null
-// which makes it hard to use. We ensure that we always have a configuration object to work with
-function getCredentialConfiguration(issuer: Issuer, credentialId: string): CredentialConfiguration {
-  const config = issuer.getCredentialConfiguration(credentialId) || {};
-
-  return {
-    ...defaultConfiguration,
-    ...config,
-  };
 }
 
 enum CredentialType {
@@ -50,17 +22,17 @@ export class OpenBadgeCredential extends BaseCredential {
   }
 
   public async generate(args: CredentialDataSupplierArgs): Promise<CredentialDataSupplierResult> {
+    // TODO: Is this the place to check if the issuer in the credential matches the issuer in the issuer?
+
     debug('OpenBadgeCredential.generate()', args);
-
-    const display = (this.issuer.metadata.display ?? [{}])[0];
-    const credentialConfiguration = getCredentialConfiguration(this.issuer, 'OpenBadgeCredential');
-    const credentialDisplay = credentialConfiguration.getFirstDisplay();
-
     const achievement = args?.credentialDataSupplierInput?.credential?.credentialSubject?.achievement ?? {};
     debug('achievement', achievement);
 
     const validFrom: string = args?.credentialDataSupplierInput?.credential?.validFrom;
     const validUntil: string | undefined = args?.credentialDataSupplierInput?.credential?.validUntil;
+
+    const issuer = args?.credentialDataSupplierInput?.credential?.issuer;
+    debug('issuer in credential', issuer);
 
     // TODO: Can the did ever be null? The sphereon types allow it, but it seems this
     // would not be a valid state in our actual badge and issuer setup. Probably
@@ -84,11 +56,11 @@ export class OpenBadgeCredential extends BaseCredential {
       ],
       issuer: {
         id: issuer_id,
-        name: display.name,
-        description: display.description,
+        name: issuer.name,
+        description: issuer.description,
       },
-      name: credentialDisplay.name,
-      description: credentialDisplay.description,
+      name: achievement.name,
+      description: achievement.description,
 
       // We add the new and the old, deprecated fields
       validFrom,
