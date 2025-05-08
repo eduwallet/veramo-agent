5f072ac5ca2995e07f099bd48b51f8984f97a957
Michiel Uitdehaag <michiel@muisit.nl>
2025-05-08 11:36:48 +0200

    Supporting JWK as proof type key material as allowed in the specs

---
+++ src/issuer/lib/getSignatureKeyFromProofJwt.ts
@@ -0,0 +1,16 @@
+import { JWT } from "#root/jwt/JWT";
+import { CryptoKey, Factory } from "@muisit/cryptokey";
+
+export async function getSignatureKeyFromProofJwt(jwt:JWT): Promise<CryptoKey|null>
+{
+    let ckey:CryptoKey|null = null;
+    if (jwt.header.kid) {
+        // do some cleanup. The trim is only needed because we have a deviant test vector
+        const kid = jwt.header.kid.split('#')[0].trim('=');
+        ckey = await Factory.resolve(kid);
+    }
+    else if(jwt.header.jwk) {
+        ckey = Factory.createFromJWK(jwt.header.jwk);
+    }
+    return ckey;
+}
