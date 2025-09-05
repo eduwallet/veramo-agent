cc161e64c2042f0de5651c0c074764905e9296a6
Michiel Uitdehaag <michiel@muisit.nl>
2025-09-05 10:31:05 +0200

    Fixed nonce endpoint. Generalised script start to yarn start

--- src/server/endpoints/getNonce.ts
+++ src/server/endpoints/getNonce.ts
@@ -2,7 +2,7 @@ import { Request, Router } from 'express'
 import { Issuer } from '#root/issuer/Issuer';
 
 export function getNonce(issuer:Issuer) {
-    issuer.router!.post('/nonce', async (req: Request, res) => {
+    issuer.router!.get('/nonce', async (req: Request, res) => {
         const nonce = await issuer.nonceStates.get('');
         const response = {
             c_nonce: nonce.uuid
