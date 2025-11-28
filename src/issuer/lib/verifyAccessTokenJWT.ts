6b2692ebae1dc67f8c1bdd267eb81edbbbdaf398
muisit <michiel@muisit.nl>
2025-11-28 10:15:07 +0100

    Adjustments to authorization_introspection

--- src/issuer/lib/verifyAccessTokenJWT.ts
+++ src/issuer/lib/verifyAccessTokenJWT.ts
@@ -19,14 +19,14 @@ export async function verifyAccessTokenJWT(token:string, issuer:Issuer)
         }
         throw new Error("Invalid JWT");
     }
-    catch (e) {
+    catch {
         if (issuer.usesAuthorisedCodeFlow()) {
             const userdata = await issuer.retrieveASIssuerIntrospection(token);
-            if (userdata && Object.keys(userdata).length > 0) {
+            if (userdata && Object.keys(userdata).length > 0 && userdata.user_info) {
                 const jwt = new JWT();
                 jwt.payload = {
-                    ...userdata['user-info'],
-                    ...(userdata['token-details'] && userdata['token-details'].issuer_state && {issuer_state: userdata['token-details'].issuer_state})
+                    ...userdata.user_info,
+                    ...(userdata.token_details && userdata.token_details.issuer_state && {issuer_state: userdata.tokn_details.issuer_state})
                 };
                 jwt.payload.iss = issuer.options?.authorizationEndpoint; // the location where we got our info
                 return jwt;
@@ -35,11 +35,4 @@ export async function verifyAccessTokenJWT(token:string, issuer:Issuer)
     }
 
     return null;
-}
-
-async function getVerificationKey(issuer:Issuer, protectedHeader:any, jws?:any): Promise<any> {
-    if (protectedHeader.kid && issuer.serverKeys[protectedHeader.kid]) {
-        return issuer.serverKeys[protectedHeader.kid];
-    }
-    return {};
 }
