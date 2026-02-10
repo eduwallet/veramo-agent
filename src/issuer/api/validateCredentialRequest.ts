b7de506dabfda144ec1a9c5ede458ff319e93260
Michiel Uitdehaag <michiel@muisit.nl>
2026-02-10 15:11:54 +0100

    Returning proof validation error in case of errors

--- src/issuer/api/validateCredentialRequest.ts
+++ src/issuer/api/validateCredentialRequest.ts
@@ -125,7 +125,7 @@ export async function validateCredentialRequest(issuer:Issuer, request:Request)
     // if we get a single ApiState back, it is the error on the proof that fails
     if (!Array.isArray(proofResults.data) && proofResults.error && proofResults.error != ErrorCodes.NO_ERROR) {
         debug("invalid proof");
-        return error;
+        return proofResults;
     }
     session.data.proofs = proofResults.data;
     await issuer.storeSession(session);
