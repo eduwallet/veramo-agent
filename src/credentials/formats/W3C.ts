9c2aaf474fc80c1a1caaf81a6aa9c179fc11088d
Michiel Uitdehaag <michiel@muisit.nl>
2026-03-03 17:22:03 +0100

    Reversed object.assign params to correct precedence

--- src/credentials/formats/W3C.ts
+++ src/credentials/formats/W3C.ts
@@ -42,7 +42,7 @@ export class W3C
 
         // if we have issuer metadata, allow enriching our basic information
         if (this.credential.metaData.issuer) {
-            baseCredential.issuer = Object.assign({}, baseCredential.issuer, this.credential.metaData.issuer);
+            baseCredential.issuer = Object.assign({}, this.credential.metaData.issuer, baseCredential.issuer);
         }
 
         // If present, id property's value MUST be a single URL, recommended to be machine readable
